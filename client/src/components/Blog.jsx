b819084e0d6d16a8bede5f54a4b7f185a2cbf21c
chiefmikey <wolfemikl@gmail.com>
2021-05-15 18:41:04 -0600

    blog

---
+++ client/src/components/Blog.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Blog = () => (
+  <div id="blog">
+    <b>wolfebyte's Latest Blog Entry</b>
+    {' '}
+    [Subscribe]
+    {'\n\n'}
+    approving comments ... (view more)
+    {'\n\n'}
+    new homepage look (view more)
+    {'\n\n'}
+    what's going on with friend counts? (view more)
+    {'\n\n'}
+    am i online? (view more)
+    {'\n\n'}
+    [View All Blog Entries]
+  </div>
+);
+
+export default Blog;
