1b7fd96512f3a7fc64c1a195f1c382fecbdd1c36
chiefmikey <wolfemikl@gmail.com>
2021-05-15 18:41:23 -0600

    contact

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => (
+  <div id="contact">
+    <div id="contact-name">
+      <h5>Contacting wolfebyte</h5>
+    </div>
+    <div id="contact-left">
+    <a href="">Send Message</a>
+      {'\n'}
+    <a href="">Add to Friends</a>
+      {'\n'}
+    <a href="">Instant Message</a>
+      {'\n'}
+    <a href="">Add to Group</a>
+    </div>
+    <div id="contact-right">
+      <a href="">Forward to Friend</a>
+      {'\n'}
+      <a href="">Add to Favorites</a>
+      {'\n'}
+      <a href="">Block User</a>
+      {'\n'}
+      <a href="">Rank User</a>
+    </div>
+  </div>
+);
+
+export default Contact;
