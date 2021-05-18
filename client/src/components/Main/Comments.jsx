8bec9c245c04dd9db80d0990daae2e00ec73ffca
chiefmikey <wolfemikl@gmail.com>
2021-05-18 15:54:53 -0600

    comments

--- client/src/components/Main/Comments.jsx
+++ client/src/components/Main/Comments.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import wbyte from '../../../public/img/wbyte.png';
+
 const Comments = () => (
   <div id="comments">
     <div id="comments-name">
@@ -7,6 +9,22 @@ const Comments = () => (
     </div>
     <div id="comments-count">
       <b>Displaying 50 of 100 comments ( View All | Add Comment )</b>
+    </div>
+    <div className="comments-post">
+      <div className="comments-post-left">
+        <div className="comments-post-left-name">
+          ding dong
+        </div>
+        <img className="comments-post-left-pic" src={wbyte} alt="comment author avatar" />
+      </div>
+      <div className="comments-post-right">
+        <div className="comments-post-right-date">
+          Sep 21 2008 5:26P
+        </div>
+        <div className="comments-post-right-content">
+          you're the best
+        </div>
+      </div>
     </div>
   </div>
 );
