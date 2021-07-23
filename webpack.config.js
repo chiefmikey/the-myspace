f94857aca8fff59218b315b901f6e437581aabf1
chiefmikey <wolfemikl@gmail.com>
2021-07-23 14:45:07 -0600

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/public');
 
+const css = 'css-loader';
+
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
@@ -24,12 +26,24 @@ module.exports = {
         use: ['source-map-loader'],
       },
       {
+        test: /\.css$/,
+        use: [css],
+      },
+      {
+        test: /\.scss$/,
+        use: [css, 'sass-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: [css, 'sass-loader'],
+      },
+      {
         test: /\.(png|ttf|jp(e*)g|svg)$/,
         use: 'url-loader?limit=100000&name=img/[name].[ext]',
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.vue', '.json', '...'],
   },
 };
