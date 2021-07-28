00b6ecb0f8ea48391e151b2415d6b0b79347346a
chiefmikey <wolfemikl@gmail.com>
2021-07-27 22:51:06 -0600

    Updated Landing component syntax

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import {
-  Router,
-  Link,
-  withRouter,
-} from 'react-router-dom';
+import { Router, Link, withRouter } from 'react-router-dom';
 
 const Landing = ({ history }) => (
   <div id="landing">
     <div id="landing-main">
       <div id="landing-main-logo">
-        <h5>
-          themyspace.org
-        </h5>
+        <h5>themyspace.org</h5>
       </div>
-      <h1>
-        GET IN MY BELLY
-      </h1>
+      <h1>GET IN MY BELLY</h1>
       <Router history={history}>
         <div id="landing-action">
-          <Link
-            to="/"
-          >
-            Sign Up
-          </Link>
+          <Link to="/">Sign Up</Link>
           {' | '}
-          <Link
-            to="/"
-          >
-            Log In
-          </Link>
+          <Link to="/">Log In</Link>
         </div>
       </Router>
     </div>
--- client/src/components/Landing/NotFound.jsx
+++ client/src/components/Landing/NotFound.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Img from 'react-cool-img';
-import {
-  Router,
-  Link,
-  withRouter,
-} from 'react-router-dom';
+import { Router, Link, withRouter } from 'react-router-dom';
 
 import loadingImage from '../../../public/img/420.jpeg';
 import errorImage from '../../../public/img/404.jpeg';
@@ -13,9 +9,7 @@ import errorImage from '../../../public/img/404.jpeg';
 const NotFound = ({ history }) => (
   <div id="not-found">
     <Router history={history}>
-      <Link
-        to="/"
-      >
+      <Link to="/">
         <Img
           id="not-found-pic-img"
           src={errorImage}
