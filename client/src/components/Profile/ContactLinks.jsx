9eed45508f924a0061c6af65e19a368a884d82f9
chiefmikey <wolfemikl@gmail.com>
2021-07-29 11:10:54 -0600

    Mapped user array of links

---
+++ client/src/components/Profile/ContactLinks.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { BiLink } from 'react-icons/bi';
+
+const ContactLinks = ({ links }) => {
+  let key = 0;
+  return links.map((link) => {
+    key += 1;
+    return (
+      <a
+        className="contact-link"
+        href={link.url}
+        target="_blank"
+        rel="noreferrer"
+        key={key}
+      >
+        <span className="contact-icon">
+          <BiLink />
+        </span>
+        {link.name}
+      </a>
+    );
+  });
+};
+
+ContactLinks.defaultProps = {
+  links: [],
+};
+
+ContactLinks.propTypes = {
+  links: propTypes.oneOfType([propTypes.array]),
+};
+
+export default ContactLinks;
