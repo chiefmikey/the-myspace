2335b0d19536b00001d6c208a86d0f48b882e847
chiefmikey <wolfemikl@gmail.com>
2021-07-29 11:10:28 -0600

    Refactored Contact component to dynamically render custom links

--- client/src/components/Profile/Contact.jsx
+++ client/src/components/Profile/Contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import LinesEllipsis from 'react-lines-ellipsis';
-
-import { BiMailSend } from 'react-icons/bi';
 import { AiOutlineUserAdd, AiOutlineUsergroupAdd } from 'react-icons/ai';
-import { RiWechatLine } from 'react-icons/ri';
+import { RiMailSendLine, RiWechatLine } from 'react-icons/ri';
+
+import ContactLinks from './ContactLinks.jsx';
 
 const Contact = ({ currentUser }) => (
   <div id="contact">
@@ -19,28 +19,45 @@ const Contact = ({ currentUser }) => (
     </h5>
     <div id="contact-layout">
       <div id="contact-left">
-        <a className="contact-link" href="http://themyspace.org">
+        <a
+          className="contact-link"
+          href={`mailto:${currentUser.email}`}
+          target="_blank"
+          rel="noreferrer"
+        >
           <span className="contact-icon">
-            <BiMailSend />
+            <RiMailSendLine />
           </span>{' '}
           Send Message
         </a>
-        {'\n'}
-        <a className="contact-link" href="http://themyspace.org">
+        <a
+          className="contact-link"
+          href="http://themyspace.org"
+          target="_blank"
+          rel="noreferrer"
+        >
           <span className="contact-icon">
             <AiOutlineUserAdd />
           </span>{' '}
           Add to Friends
         </a>
-        {'\n'}
-        <a className="contact-link" href="http://themyspace.org">
+        <a
+          className="contact-link"
+          href="http://themyspace.org"
+          target="_blank"
+          rel="noreferrer"
+        >
           <span className="contact-icon">
             <RiWechatLine />
           </span>{' '}
           Instant Message
         </a>
-        {'\n'}
-        <a className="contact-link" href="http://themyspace.org">
+        <a
+          className="contact-link"
+          href="http://themyspace.org"
+          target="_blank"
+          rel="noreferrer"
+        >
           <span className="contact-icon">
             <AiOutlineUsergroupAdd />
           </span>{' '}
@@ -48,21 +65,7 @@ const Contact = ({ currentUser }) => (
         </a>
       </div>
       <div id="contact-right">
-        <a className="contact-link" href={currentUser.links.link1.url}>
-          {currentUser.links.link1.name}
-        </a>
-        {'\n'}
-        <a className="contact-link" href={currentUser.links.link1.url}>
-          {currentUser.links.link2.name}
-        </a>
-        {'\n'}
-        <a className="contact-link" href={currentUser.links.link1.url}>
-          {currentUser.links.link3.name}
-        </a>
-        {'\n'}
-        <a className="contact-link" href={currentUser.links.link1.url}>
-          {currentUser.links.link4.name}
-        </a>
+        <ContactLinks links={currentUser.links} />
       </div>
     </div>
   </div>
