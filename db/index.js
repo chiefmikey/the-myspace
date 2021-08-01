03f62997da4a8356736ff52fd28547d432770513
chiefmikey <wolfemikl@gmail.com>
2021-07-31 23:05:36 -0600

    Prepared mongoose for connection

--- db/index.js
+++ db/index.js
@@ -1,3 +1,23 @@
+// import mongoose from 'mongoose';
+
+// const mongoURI = 'mongodb://127.0.0.1:27017/myspace';
+
+// const db = mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+
+// db.then(() => console.log(`Connected to: ${mongoURI}`)).catch((error) => {
+//   console.error(
+//     `There was a problem connecting to mongo at: ${mongoURI}`,
+//     error,
+//   );
+// });
+
+// export default db;
+
 // const mongoose = require('mongoose');
 
 // const { usersSchema } = require('./schemas/users');
