ac145f9fcf91c170b7bea4a14de87a1b6eae431d
chiefmikey <wolfemikl@gmail.com>
2021-09-29 16:07:57 -0600

    Refactor content list to page

---
+++ client/src/components/Content/ContentAll.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContentPostList from './ContentPostList.jsx';
+
+const ContentAll = (history, currentUser, currentPost, selectPost) => (
+  <ContentPostList
+    history={history}
+    currentUser={currentUser}
+    selectPost={selectPost}
+    currentPost={currentPost}
+  />
+);
+
+export default ContentAll;
