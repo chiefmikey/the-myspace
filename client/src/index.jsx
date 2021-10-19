01d6d61426f3e9ff5113d2369d88230b62f1b690
chiefmikey <wolfemikl@gmail.com>
2021-10-19 14:29:11 -0600

    Init redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
 import App from './components/App.jsx';
+import configureStore from './redux/configureStore';
 import './styles.css';
 
 const history = createBrowserHistory();
+const store = configureStore();
 
-ReactDOM.render(<App history={history} />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
+  document.getElementById('app'),
+);
